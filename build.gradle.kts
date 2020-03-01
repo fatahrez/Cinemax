90f58326695076bb8a564c7a661ca3c7ce076988
fatahrez <fertahrez@gmail.com>
2020-03-02 00:34:44 +0300

    create module buildSrc for managing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.0-beta01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -21,6 +21,7 @@ allprojects {
 
     }
 }
+
 tasks {
     val clean by registering(Delete::class) {
         delete(buildDir)
---
+++ buildSrc/src/main/java/Dep.kt
@@ -0,0 +1,7 @@
+object Versions {
+    const val kotlin = "1.3.61"
+}
+
+object Dep {
+
+}
