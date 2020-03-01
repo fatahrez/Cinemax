9def073f21687235121743d31ee2d44476076a89
fatahrez <fertahrez@gmail.com>
2020-03-02 00:21:24 +0300

    migrate build.gradle(project) and build.gradle(app) to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.3.61"
     repositories {
         google()
         jcenter()
 
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.0.0-alpha08"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.0.0-beta01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,7 +21,8 @@ allprojects {
 
     }
 }
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
