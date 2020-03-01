99337858408ac2266dcc7f23d2e85775d3dc3251
fatahrez <fertahrez@gmail.com>
2020-03-02 00:38:34 +0300

    move dependencies for GradlePluginsto Dep.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0-beta01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath(Dep.GradlePlugins.android_tools_gradle)
+        classpath(Dep.GradlePlugins.kotlin_gradle)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
--- buildSrc/src/main/java/Dep.kt
+++ buildSrc/src/main/java/Dep.kt
@@ -1,7 +1,11 @@
 object Versions {
     const val kotlin = "1.3.61"
+    const val tools_gradle = "4.0.0-beta01"
 }
 
 object Dep {
-
+    object GradlePlugins {
+        val kotlin_gradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+        val android_tools_gradle = "com.android.tools.build:gradle:${Versions.tools_gradle}"
+    }
 }
