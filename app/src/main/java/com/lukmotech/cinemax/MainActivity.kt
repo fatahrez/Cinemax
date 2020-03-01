bacad46b7f61cc6803770979403de8cbea9c06e3
fatahrez <fertahrez@gmail.com>
2020-03-02 00:07:34 +0300

    start project migrate settings.gradle to settings.gradle.kts

---
+++ app/src/main/java/com/lukmotech/cinemax/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lukmotech.cinemax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
